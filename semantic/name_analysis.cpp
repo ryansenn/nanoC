433779a40876022bbe77bdad7ca2db49057ef40d
ryan <ryansen87@gmail.com>
2024-06-30 21:32:46 -0400

    reorg + test

---
+++ semantic/name_analysis.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Ryan Senoune on 2024-06-30.
+//
---
+++ semantic/name_analysis.h
@@ -0,0 +1,8 @@
+//
+// Created by Ryan Senoune on 2024-06-30.
+//
+
+#ifndef COMPILER_NAME_ANALYSIS_H
+#define COMPILER_NAME_ANALYSIS_H
+
+#endif //COMPILER_NAME_ANALYSIS_H
