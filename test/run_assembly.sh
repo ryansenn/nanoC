6a570199e83581d1caec5978994bf57ab4f070ed
ryan <ryansen87@gmail.com>
2024-07-08 13:01:04 -0400

    fixed linker

--- test/run_assembly.sh
+++ test/run_assembly.sh
@@ -4,7 +4,8 @@
 
 nasm -f macho64 -o output.o ../cmake-build-debug/output.asm
 
-clang -arch x86_64 -o output output.o
+#clang -arch x86_64 -o output output.o
+ld -o output output.o -macosx_version_min 10.7 -no_pie
 
 chmod +x ./output
 
