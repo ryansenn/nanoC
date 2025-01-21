9544bf30546fc9ba428dd8bee50bf6bad5b11164
ryan <ryansen87@gmail.com>
2025-01-21 11:25:52 -0500

    code gen while test

---
+++ test/code_gen/while.c
@@ -0,0 +1,24 @@
+/*
+7
+21
+*/
+#include <print>
+
+int main(){
+    int a;
+    a = 0;
+
+    int i;
+    i = 0;
+
+    while (i < 7){
+        a = a + i;
+        i = i + 1;
+    }
+
+    print_i(i);
+    print_c('\n');
+    print_i(a);
+
+    return 0;
+}
