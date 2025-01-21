91853520460acdbd75851de25910cb1988565444
ryan <ryansen87@gmail.com>
2025-01-21 11:23:02 -0500

    code gen tests for assignment and binary operations

---
+++ test/code_gen/assignment.c
@@ -0,0 +1,16 @@
+/*
+1234a
+*/
+#include <print>
+
+int main(){
+    int a;
+    a = 1234;
+    print_i(a);
+
+    char c;
+    c = 'a';
+    print_c(c);
+
+    return 0;
+}
---
+++ test/code_gen/binary_operations.c
@@ -0,0 +1,42 @@
+/*
+7
+6
+10
+111110
+*/
+#include <print>
+
+int main(){
+    int a;
+    a = 1+6;
+    print_i(a);
+    print_c('\n');
+
+    a = 9 - 3;
+    print_i(a);
+    print_c('\n');
+
+    a = 2 * 5;
+    print_i(a);
+    print_c('\n');
+
+    a = 1 < 2;
+    print_i(a);
+
+    a = 2 > 1;
+    print_i(a);
+
+    a = 1 >= 1;
+    print_i(a);
+
+    a = 1 <= 1;
+    print_i(a);
+
+    a = 1 == 1;
+    print_i(a);
+
+    a = 1 != 1;
+    print_i(a);
+
+    return 0;
+}
