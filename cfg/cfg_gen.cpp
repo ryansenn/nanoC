ef60010725dcb9ff7d07a52b446fd00553b59b26
ryan <ryansen87@gmail.com>
2025-01-30 19:36:24 -0500

    cfg gen: refactoring

--- cfg/cfg_gen.cpp
+++ cfg/cfg_gen.cpp
@@ -6,3 +6,71 @@
 
 int VirtualRegister::count = 0;
 int BasicBlock::count = 0;
+
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Program> p) {
+    for (auto d : p->decls){
+        d->accept(*this);
+    }
+}
+
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<FuncDecl> f) {
+    curr_cfg = std::make_shared<CFG>(f->name);
+    f->block->accept(*this);
+    results.push_back(curr_cfg);
+}
+
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Block> b) {
+    for (auto s : b->stmts){
+        s->accept(*this);
+    }
+}
+
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Primary> p) {
+
+}
+
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Return> ret) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<If> ifStmt) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<While> whileStmt) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Break> breakStmt) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Continue> continueStmt) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<VarDecl> varDecl) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Subscript> subscript) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Member> member) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Call> call) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Unary> unary) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<TypeCast> typeCast) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Binary> binary) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<Type> type) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<FunProto> funProto) {
+
+}
+std::shared_ptr<VirtualRegister> cfg_gen::visit(std::shared_ptr<StructDecl> structDecl) {
+
+}
--- cfg/cfg_gen.h
+++ cfg/cfg_gen.h
@@ -47,73 +47,25 @@ public:
     std::shared_ptr<CFG> curr_cfg;
     std::shared_ptr<BasicBlock> curr_block;
 
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Program> p) override {
-        for (auto d : p->decls){
-            d->accept(*this);
-        }
-    }
-
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<FuncDecl> f) override {
-        curr_cfg = std::make_shared<CFG>(f->name);
-        f->block->accept(*this);
-        results.push_back(curr_cfg);
-    }
-
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Block> b) override {
-        for (auto s : b->stmts){
-            s->accept(*this);
-        }
-    }
-
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Primary> primary) override {
-
-    }
-
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Return> ret) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<If> ifStmt) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<While> whileStmt) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Break> breakStmt) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Continue> continueStmt) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<VarDecl> varDecl) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Subscript> subscript) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Member> member) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Call> call) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Unary> unary) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<TypeCast> typeCast) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Binary> binary) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Type> type) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<FunProto> funProto) override {
-
-    }
-    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<StructDecl> structDecl) override {
-
-    }
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Program>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<FuncDecl>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Block>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Return>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<If>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<While>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Break>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Continue>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<VarDecl>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Subscript>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Member>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Call>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Primary>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Unary>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<TypeCast>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Binary>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<Type>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<FunProto>) override;
+    std::shared_ptr<VirtualRegister> visit(std::shared_ptr<StructDecl>) override;
 };
 
 
