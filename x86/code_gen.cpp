b4c6bfbb15d5ac9f82f5556bc60daad4ed4f9373
ryan <ryansen87@gmail.com>
2025-02-03 23:39:13 -0500

    ir code gen

--- x86/code_gen.cpp
+++ x86/code_gen.cpp
@@ -12,11 +12,28 @@ void CodeGen::generate(){
 
         while (curr()->opcode == "global"){
             file << curr()->label << ": resq " << std::to_string(std::stoi(curr()->value)/8) << std::endl;
+            index++;
         }
-
     }
 
     file << "section .text" << std::endl;
+    file << "global main" << std::endl;
+    file << "main:" << std::endl;
+
+
+    file << "    push rbp" << std::endl;
+    file << "    mov rbp, rsp" << std::endl;
+
+
+    while (index < instructions.size()) {
+
+        index++;
+    }
+
+
+    file << "    mov rsp, rbp" << std::endl;
+    file << "    pop rbp" << std::endl;
+    file << "    ret" << std::endl;
 
 
 }
